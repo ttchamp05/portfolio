9dc245ad69cc8bf107e4a61843ab9a34766f1600
ttchamp05 <kurubakaustubh123@gmail.com>
2019-11-19 17:32:11 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Kaustubh Kuruba Period:2</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Kuruba_AboutMe.html">AboutMe</a></li></br>
+      <li><a href="Kuruba_Projects.html">Projects</a></li></br>
+      <li><a href="Kuruba_Resume.html">Resume</a></li></br>
+      <li><a href="Kuruba_ContactMe.html">ContactMe</a></li></br>
+      
+    <ul>
+  </body>
+</html>
